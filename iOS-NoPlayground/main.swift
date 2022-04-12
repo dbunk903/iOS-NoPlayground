3a7379b829e2d4a7e11e8f9b08debb494c31059a
dbunk903 <dbunk903@gmail.com>
2022-04-12 10:59:31 +0900

    Inherit attack(), override previous super function, add and modify functions

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,13 +1,10 @@
 // Class Demo
 
 let skeleton = Enemy()
-print(skeleton.health)
-skeleton.move()
-skeleton.attack()
-
 let dragon = Dragon()
 dragon.wingSpan = 5
-print(dragon.wingSpan)
+dragon.attackStrength = 15
+dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are a hurricane!")
 dragon.move()
 dragon.attack()
 
