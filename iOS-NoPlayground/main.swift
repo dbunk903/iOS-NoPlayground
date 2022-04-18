fc4153d9b6f2dfb49d8ed043202f5e1ea2fc7ea6
dbunk903 <dbunk903@gmail.com>
2022-04-18 09:33:56 +0900

    String and String Combination

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,1 +1,13 @@
+let string1 = "hello"
+let string2 = " there"
+var welcome = string1 + string2
+// welcome : "hello there"
+print(welcome)
 
+var instruction = "look over"
+instruction += string2
+print(instruction)
+
+let exclamationMark: Character = "!"
+welcome.append(exclamationMark)
+print(welcome)
