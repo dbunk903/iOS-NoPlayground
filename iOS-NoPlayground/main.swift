81543d69c6581e5f3b84350ecd0cb6504b3cf231
dbunk903 <dbunk903@gmail.com>
2022-04-18 09:58:13 +0900

    String Insertions and removal

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,4 +1,13 @@
-let greeting = "Hello Man"
-for index in greeting.indices{
-    print("\(greeting[index])", terminator: "")
-}
+var welcome = "hello"
+welcome.insert("!", at:welcome.endIndex)
+print(welcome)
+
+welcome.insert(contentsOf: " there", at: welcome.index(before: welcome.endIndex))
+print(welcome)
+
+welcome.remove(at: welcome.index(before: welcome.endIndex))
+print(welcome)
+
+let range = welcome.index(welcome.endIndex, offsetBy: -6)..<welcome.endIndex
+welcome.removeSubrange(range)
+print(welcome)
