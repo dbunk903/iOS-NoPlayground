6df0c158b2e13f32734323a560d10c77f2ff1d94
dbunk903 <dbunk903@gmail.com>
2022-04-28 14:24:50 +0900

    Optional Check for nil value

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,14 +1,9 @@
-import Foundation
-var shoppingList = ["Eggs","Milk"]
-print(shoppingList.isEmpty)
-shoppingList.append("Four")
-// shoppingList.count = 3
-shoppingList += ["Baking Powder"]
-// shoppingList.count = 4
-shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
-// shoppingList.count = 7
-print(shoppingList[4...6])
-shoppingList.insert("Maple Syrup", at: 0)
-print(shoppingList)
-let mapleSyrup = shoppingList.remove(at: 0)
-print(mapleSyrup)
+let myOptional: String?
+
+myOptional = nil
+
+if myOptional != nil {
+    let text: String = myOptional!
+}else {
+    print("myOptional is nil")
+}
