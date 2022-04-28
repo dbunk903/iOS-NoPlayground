bea9120822d039ce8f1e67467153a13ec575f1de
dbunk903 <dbunk903@gmail.com>
2022-04-28 09:58:10 +0900

    Unicode Scalar

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,33 +1,15 @@
-let romeoAndJuliet = [
-    "Act 1 Scene 1: Verona, A public place",
-    "Act 1 Scene 2: Capulet's mansion",
-    "Act 1 Scene 3: A room in Capulet's mansion",
-    "Act 1 Scene 4: A street outside Capulet's mansion",
-    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
-    "Act 2 Scene 1: Outside Capulet's mansion",
-    "Act 2 Scene 2: Capulet's orchard",
-    "Act 2 Scene 3: Outside Friar Lawrence's cell",
-    "Act 2 Scene 4: A street in Verona",
-    "Act 2 Scene 5: Capulet's mansion",
-    "Act 2 Scene 6: Friar Lawrence's cell"
-]
-
-var act1SceneCount = 0
-for scene in romeoAndJuliet {
-    if scene.hasPrefix("Act 1"){
-        act1SceneCount += 1
-    }
+let dogString = "Dog!!🐶"
+for codeUnit in dogString.utf8 {
+    print("\(codeUnit) ", terminator: "")
 }
-print("There are \(act1SceneCount) scenes in Act 1")
+print("")
+// 68 111 103 226 128 188 240 159 144 182
 
-var mansionCount = 0
-var cellCount = 0
-for scene in romeoAndJuliet {
-    if scene.hasSuffix("Capulet's mansion"){
-        mansionCount += 1
-    }else if scene.hasSuffix("Friar Lawrence's cell"){
-        cellCount += 1
-    }
+for scalar in dogString.unicodeScalars {
+    print("\(scalar) ")
 }
-print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
-// 6 mansion scenes; 2 cell scenes
+// D
+// o
+// g
+// !!
+// 🐶
