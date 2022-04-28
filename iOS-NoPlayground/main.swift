b89eaba8bdf4311b9a314b23f650f854e4c9aa38
dbunk903 <dbunk903@gmail.com>
2022-04-28 10:17:02 +0900

    Array Manipulation

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,10 +1,14 @@
-var someInts = [Int]()
-print(someInts)
-someInts.append(8)
-print(someInts)
-
-var threeDoubles = Array(repeating: 0.0, count: 3)
-print(threeDoubles)
-
-var anotherDoubles = Array(repeating: 3.3, count: 3)
-print(threeDoubles + anotherDoubles)
+import Foundation
+var shoppingList = ["Eggs","Milk"]
+print(shoppingList.isEmpty)
+shoppingList.append("Four")
+// shoppingList.count = 3
+shoppingList += ["Baking Powder"]
+// shoppingList.count = 4
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
+// shoppingList.count = 7
+print(shoppingList[4...6])
+shoppingList.insert("Maple Syrup", at: 0)
+print(shoppingList)
+let mapleSyrup = shoppingList.remove(at: 0)
+print(mapleSyrup)
