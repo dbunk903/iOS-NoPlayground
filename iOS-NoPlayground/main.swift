a3086ee95dfa95e61fb111d1e3a2a9d1ba3342b9
dbunk903 <dbunk903@gmail.com>
2022-04-29 09:52:56 +0900

    Jeong - For Loop

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,35 +1,16 @@
-// 학교 - 초, 중, 고
-enum School {
-//    case elementary
-//    case middle
-//    case high
-    case elementary, middle, high
+for i in 0..<5{
+    print(i)
+}
+print("---------------------------")
+for i in 0...5 where i%2 == 0{
+    print(i)
 }
 
-let yourSchool = School.high
-//print(yourSchool)
-print("yourSchool: ",yourSchool)
 
-enum Grade:Int {
-    case first = 1
-    case second = 2
+var randomInts = [Int]()
+
+for _ in 0..<25 {
+    let randomNumber = Int.random(in: 1...100)
+    randomInts.append(randomNumber)
+    print(randomInts)
 }
-
-let yourGrade = Grade.second
-print("yourGrade: ",yourGrade.rawValue)
-
-enum SchoolDetail {
-    case elementary(name: String)
-    case middle(name: String)
-    func get() -> String{
-        switch self {
-        case .elementary(let name):
-            return name
-        case .middle(let name):
-            return name
-        }
-    }
-}
-
-let yourMiddleSchoolName = SchoolDetail.middle(name: "정대리중학교")
-print(yourMiddleSchoolName.get())
