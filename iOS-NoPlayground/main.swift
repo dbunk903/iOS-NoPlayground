f8cc3663803f36949b49567d1712542b5497655b
dbunk903 <dbunk903@gmail.com>
2022-05-05 08:40:50 +0900

    SLG - Set Access and modifications

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,8 +1,31 @@
-var letters = Set<Character>()
-print("letters is of type Set<Character> with \(letters.count) items.")
+var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
 
-letters.insert("a")
-print(letters)
-// ["a"]
+print("I have \(favoriteGenres.count) favorite music genres.")
 
+//I have 3 favorite music genres.
 
+if favoriteGenres.isEmpty{
+    print("As far as music goes, I'm not picky.")
+} else {
+    print("I have particular music preferences.")
+}
+
+favoriteGenres.insert("Jazz")
+
+if let removedGenre = favoriteGenres.remove("Rock") {
+    print("\(removedGenre)? I'm over it.")
+} else {
+    print("I never much cared for that.")
+}
+
+//Rock? I am over it.
+
+print(favoriteGenres)
+
+if favoriteGenres.contains("Funk"){
+    print("I get up on the good foot.")
+} else {
+    print("It's too funky in here")
+}
+
+// It's too funky in here
