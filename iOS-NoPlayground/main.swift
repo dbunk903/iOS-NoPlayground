5d51f541c10e650aca1817a198227f93e41de4ff
dbunk903 <dbunk903@gmail.com>
2022-05-09 12:01:45 +0900

    SLG - Dictionary assigns value

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -3,3 +3,12 @@ print(airports)
 
 var airport = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
 print(airport)
+
+if airports.isEmpty {
+    print("empty")
+} else{
+    print("not empty")
+}
+
+airports["LHR"] = "London"
+//the airpots dictionary now contains 3 items
