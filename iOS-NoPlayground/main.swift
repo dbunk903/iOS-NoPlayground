9f40e1afb54a733ead78151584d74d62779f3cd7
dbunk903 <dbunk903@gmail.com>
2022-05-10 11:32:07 +0900

    JJeong - Generic Example

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,11 +1,32 @@
-let someCharacter: Character = "e"
-switch someCharacter {
-case "a", "e", "i", "o", "u":
-    print("\(someCharacter) is a vowel")
-case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
-     "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
-    print("\(someCharacter) is a consonant")
-default:
-    print("\(someCharacter) is not a vowel or a consonant")
+struct MyArray<T>{
+    
+    // 제네릭을 담은 빈 배열
+    var elements : [T] = [T]()
+    
+    // 생성자
+    init(_ elements: [T]){
+        self.elements = elements
+    }
+    
 }
-// Prints "e is a vowel"
+
+struct Friend {
+    var name: String
+}
+
+struct PpakCoder {
+    var name: String
+}
+
+var mySomeArray = MyArray([1,2,3])
+print("mySomeArray : \(mySomeArray)")
+
+var myStringArray = MyArray(["가","나","다"])
+print("myStringArray : \(myStringArray)")
+
+let friend_01 = Friend(name: "철수")
+let friend_02 = Friend(name: "영희")
+let friend_03 = Friend(name: "수잔")
+
+var myFriendsArray = MyArray([friend_01,friend_02,friend_03])
+print("myFriendsArray : \(myFriendsArray)")
