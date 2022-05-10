c2348328a9f6d335a2d716a294c3d3039243358a
dbunk903 <dbunk903@gmail.com>
2022-05-10 10:08:11 +0900

    SLG - for- in using _

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,16 +1,8 @@
-let names = ["Anna", "Alex", "Brian", "Jack"]
-for name in names {
-    print("Hello, \(name)!")
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power {
+    answer *= base
 }
-// Hello, Anna!
-// Hello, Alex!
-// Hello, Brian!
-// Hello, Jack!
 
-let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
-for (animalName, legCount) in numberOfLegs {
-    print("\(animalName)s have \(legCount) legs")
-}
-// ants have 6 legs
-// spiders have 8 legs
-// cats have 4 legs
+print("\(base) to the power of \(power) is \(answer)")
