2e136e9fd8a0ec4bc290fcbc1a62db61069aeed2
dbunk903 <dbunk903@gmail.com>
2022-05-10 10:21:37 +0900

    SLG - Switch Interval

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,8 +1,19 @@
-let base = 3
-let power = 10
-var answer = 1
-for _ in 1...power {
-    answer *= base
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+let naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
 }
-
-print("\(base) to the power of \(power) is \(answer)")
+print("There are \(naturalCount) \(countedThings).")
+// Prints "There are dozens of moons orbiting Saturn."
