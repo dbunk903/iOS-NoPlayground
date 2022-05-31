d40151b1aa706e83c714f0b4ea384b59000b8299
dbunk903 <dbunk903@gmail.com>
2022-05-31 21:09:00 +0900

    Closure Example

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,22 +1,10 @@
-class Solution {
-    func addStrings(_ num1: String, _ num2: String) -> String {
-        var num1 = Array(num1)
-        var num2 = Array(num2)
-        
-        var carry = 0
-        var result = ""
-        while !num1.isEmpty || num2.isEmpty{
-            let val1 = num1.isEmpty ? "0" : num1.removeLast()
-            let val2 = num2.isEmpty ? "0" : num2.removeLast()
-            
-            let sum = Int(String(val1))! + Int(String(val2))! + carry
-            
-            result.append("\(sum%10)")
-            carry = sum / 10
-        }
-        if carry != 0{
-            result.append("\(carry)")
-        }
-        return String(result.reversed())
-    }
+import Foundation
+func calculator (n1 : Int, n2: Int, operation: (Int,Int) -> Int) -> Int{
+    return operation(n1,n2)
 }
+func add (n1: Int, n2:Int) -> Int{
+    return n1+n2
+}
+
+
+print(calculator(n1: 2, n2: 4, operation: {(n1:Int, n2:Int) -> Int in return n1+n2}))
