a0ffaafb6ca7b2dc655b0c87bf7a98adffe00061
dbunk903 <dbunk903@gmail.com>
2022-05-31 21:34:07 +0900

    Closure Map Example

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,13 +1,9 @@
-import Foundation
-func calculator (n1 : Int, n2: Int, operation: (Int,Int) -> Int) -> Int{
-    return operation(n1,n2)
-}
-func add (n1: Int, n2:Int) -> Int{
-    return n1+n2
+let array = [6,2,3,9,4,1]
+
+func addOne (n1:Int) -> Int{
+    return n1 + 1
 }
 
-
-print(calculator(n1: 2, n2: 4, operation: {(n1:Int, n2:Int) -> Int in return n1+n2}))
-print(calculator(n1: 2, n2: 4, operation: {$0 * $1}))
-print(calculator(n1: 2, n2: 4){$0 * $1})
-
+print(array.map(addOne))
+print(array.map{$0*0})
+print(array.map({(n1) in n1+1}))
