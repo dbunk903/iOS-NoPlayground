43c6c03669c50fa2bff7ecabd74a4a84409744ff
dbunk903 <dbunk903@gmail.com>
2022-06-07 16:49:13 +0900

    [Swift Deep Dive] extension example

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,32 +1,19 @@
 import Foundation
 
-public func solution(_ A : inout [Int]) -> Int {
-    var minIdx = 0
-    var minAvg:Float = Float(A.reduce(0, +))
-    print(minAvg)
-    for i in 2...A.count-1{
-        print("i : \(i)")
-        // three elements
-        var avg : Float = Float(A[i-2] + A[i-1] + A[i]) / 3.0
-        if avg < minAvg{
-            minIdx = i-2
-            minAvg = avg
-            print("3 minidx : \(minIdx)")
-            print("3 minavg : \(minAvg)")
-        }
-        // two elements
-        avg = Float(A[i-1] + A[i]) / 2.0
-        if avg < minAvg{
-            minIdx = i-1
-            minAvg = avg
-            print("2 minidx : \(minIdx)")
-            print("2 minavg : \(minAvg)")
-        }
+var myDouble = 3.141592
+
+// Round "to" 를 만들고 싶다면...
+
+extension Double{
+    func round(to places: Int) -> Double{
+        let precisionNumber = pow(10,Double(places))
+        var n = self
+        n = n * precisionNumber
+        n.round()
+        n = n / precisionNumber
+        return n
     }
-    return minIdx
 }
 
-
-var A = [4, 2, 2, 5, 1, 5, 8]
-print(solution(&A))
+ print(myDouble.round(to: 2))
 
