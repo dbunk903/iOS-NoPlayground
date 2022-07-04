9841616cb0691fc6195bfa4757c162c5bc67db08
dbunk903 <dbunk903@gmail.com>
2022-07-04 13:03:05 +0900

    Protocol Example

--- iOS-NoPlayground/main.swift
+++ iOS-NoPlayground/main.swift
@@ -1,19 +1,37 @@
-import Foundation
-
-var myDouble = 3.141592
-
-// Round "to" 를 만들고 싶다면...
-
-extension Double{
-    func round(to places: Int) -> Double{
-        let precisionNumber = pow(10,Double(places))
-        var n = self
-        n = n * precisionNumber
-        n.round()
-        n = n / precisionNumber
-        return n
+import Darwin
+class Bird {
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale{
+            print("the bird makes a new bird in a shell.")
+        }
+    }
+    func fly(){
+        print("두 날개로 훨훨 난다")
     }
 }
 
- print(myDouble.round(to: 2))
+class Eagle : Bird{
+    func soar(){
+        print("독수리가 박차오른다")
+    }
+}
 
+class Penguin: Bird{
+    func swim(){
+        print("펭귄이 첨벙첨벙 헤엄친다")
+    }
+}
+
+let myEagle = Eagle()
+myEagle.soar()
+
+struct FlyingMuseum{
+    func anObjectFlying(flyingObject:Bird){
+        flyingObject.fly()
+    }
+}
+
+let myMuseum = FlyingMuseum()
+myMuseum.anObjectFlying(flyingObject: myEagle)
